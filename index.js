a10024278d73208010ef2af4183756c9d4fff063
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-06-27 13:58:24 +0800

    Add es6 to default envs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { extends: extensions, parserOptions, rules } = require('./.eslintrc.json');
 
 module.exports = {
+  env: {
+    es6: true,
+  },
   extends: extensions,
   parserOptions,
   rules,
